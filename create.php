2d2dfe5ba714fee95c9817361921d398fad5c86b
Mukesh Mahato <55306415+mukeshmahato843@users.noreply.github.com>
2020-03-18 17:44:55 +0545

    Update create.php

--- create.php
+++ create.php
@@ -3,10 +3,7 @@
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
 
-}
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
